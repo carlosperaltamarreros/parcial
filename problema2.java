c26d53dd3b3f0c63fefb984b94640509d6b2bc6f
orson98 <carlosperaltamarreros>
2026-05-18 23:41:14 -0500

    problema2-main

---
+++ problema2.java
@@ -0,0 +1,24 @@
+//diseña un algoritmo utilizando 
+//duvude y venceras que encuentre el segundo elemnto
+// mas grande del arreglo de enteros
+//ejemplo:[8,3,10,5,7]
+//resultado esperado: 8
+
+//REQUISITOS
+//diseñando el algoritmo usando divide y venceras 
+//ecplicar como se combina los resultados 
+//obtener T(n)
+//analizar la complejidad temporal big O
+public class problema2 {
+    public static void main(String[] args){
+        int[] arr = {8,3,10,5,7};
+        //int n=5;
+        int n = arr.length;
+
+        //int res = mayo2(arr, 0, n-1);
+        System.out.println("{8,3,10,5,7");
+        System.out.println("el 2do mayor es : ");
+
+    }
+    
+}
