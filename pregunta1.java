c24929d9482967c0dbfbbf36f1becd1a8a5c0f75
orson98 <carlosperaltamarreros>
2026-05-18 23:12:13 -0500

    t(n)-big(O)-no se guardo

--- pregunta1.java
+++ pregunta1.java
@@ -7,17 +7,33 @@
 
 //
 
- 
+//t(n) = t(n-1) + c
+//t(n) = c*n + c0
+//porque se hace una llamada recursiva y se hace una comparacion, 
+//cada vez que entra a la funcion se hace una comparacion que 
+//que cuesta un tiempo constante, como el arreglo se va reduciendo en cada llamada recursiva,
+//el tiempo total es proporcional a n, por lo tanto la complejidad es O(n).
+//
+// otro ejemplo si en caso sea n=3
+//t(3) = t(2) + c
+//t(2) = t(1) + c
+//t(1) = c0
+
 
 public class pregunta1 {
 
     public static boolean ordenado(int[] arr, int i, int n){
     //caso base
+    //tiempo constante 
     if(i == n-1){
+        
         return true;
     }
     else{
         //comparacion
+        //tiempo constante  
+        //si el elemento actual es mayor que el siguiente,
+        //entonces el arreglo no esta ordenado
         if(arr[i] > arr[i+1]){
             return false;
         }
