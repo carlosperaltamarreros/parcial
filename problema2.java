b421556b64281d3891108f1cb838c4aa35a534b6
orson98 <carlosperaltamarreros>
2026-05-19 01:20:32 -0500

    bus-mayo2

--- problema2.java
+++ problema2.java
@@ -10,32 +10,21 @@
 //obtener T(n)
 //analizar la complejidad temporal big O
 public class problema2 {
-    public static int mayo2(int[] arr, int inicio , int fin){
-        //caso base
+
+    public static int busmaxreal(int[] arr, int inicio, int fin){
+    
+        int medio;
+        int max1;
+        int max2;
+
         if(inicio == fin){
-            return arr[inicio]; 
+            return arr[inicio];
         }
-        
-        /*
-        error, envez que me salga 8 me sale 10. 
-        for(int i = inicio; i < fin; i++){
-            if(arr[i] > arr[i+1]){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
-        }
-        return arr[fin]; */
-
-        
-        //divido el arreglo en dos partes
-        //llamo recursivamente a cada mitad
-
         else{
-            int mitad = (inicio + fin) / 2;
-            int max1 = mayo2(arr, inicio, mitad);
-            int max2 = mayo2(arr, mitad + 1, fin);
-            //combinar resultados
+            medio = (inicio + fin) / 2;
+            max1 = busmaxreal(arr, inicio, medio);
+            max2 = busmaxreal(arr, medio + 1, fin);
+            //combinar los resultados
             if(max1 > max2){
                 return max1;
             }
@@ -44,14 +33,50 @@ public class problema2 {
             }
         }
     }
+
+    public static int mayo2(int[] arr, int inicio, int fin, int maxAbsoluto){
+        int medio;
+        int max1;
+        int max2;
+
+        if(inicio == fin){
+            //si el elemento actual es igual al maximo absoluto, entonces no se considera
+            if(arr[inicio] == maxAbsoluto){
+                return Integer.MIN_VALUE; //retorna el valor minimo para indicar que no se considera
+            }
+            else{
+                return arr[inicio];
+            }
+        }
+        else{
+            medio = (inicio + fin) / 2;
+            max1 = mayo2(arr, inicio, medio, maxAbsoluto);
+            max2 = mayo2(arr, medio + 1, fin, maxAbsoluto);
+            //combinar los resultados
+            if(max1 > max2){
+                return max1;
+            }
+            else{
+                return max2;
+            }
+        }
+    }
+        
+    
+    
     public static void main(String[] args){
         int[] arr = {8,3,10,5,7};
         int n = arr.length;
 
-        int res = mayo2(arr, 0, n-1);
-        System.out.println("{8,3,10,5,7}");
-        System.out.println("el 2do mayor es : " + res);
+        //encontrar el maximo absoluto del arreglo
+        int maxAbsoluto = busmaxreal(arr, 0, n-1);
+        int res = mayo2(arr, 0, n-1, maxAbsoluto);
 
-    }
-    
+
+        System.out.println("{8,3,10,5,7}");
+        System.out.println("el 1ernum mayor esas : " + maxAbsoluto);
+        System.out.println("el 2ndo num mayor es : " + res);
 }
+}
+
+
