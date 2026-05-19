6af5b0297735b753b495763a5f6bb117094cab38
orson98 <carlosperaltamarreros>
2026-05-19 00:05:32 -0500

    mayo2

--- problema2.java
+++ problema2.java
@@ -10,14 +10,47 @@
 //obtener T(n)
 //analizar la complejidad temporal big O
 public class problema2 {
+    public static int mayo2(int[] arr, int inicio , int fin){
+        //caso base
+        if(inicio == fin){
+            return arr[inicio]; 
+        }
+        
+        /*
+        error, envez que me salga 8 me sale 10. 
+        for(int i = inicio; i < fin; i++){
+            if(arr[i] > arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        return arr[fin]; */
+
+        
+        //divido el arreglo en dos partes
+        //llamo recursivamente a cada mitad
+
+        else{
+            int mitad = (inicio + fin) / 2;
+            int max1 = mayo2(arr, inicio, mitad);
+            int max2 = mayo2(arr, mitad + 1, fin);
+            //combinar resultados
+            if(max1 > max2){
+                return max1;
+            }
+            else{
+                return max2;
+            }
+        }
+    }
     public static void main(String[] args){
         int[] arr = {8,3,10,5,7};
-        //int n=5;
         int n = arr.length;
 
-        //int res = mayo2(arr, 0, n-1);
-        System.out.println("{8,3,10,5,7");
-        System.out.println("el 2do mayor es : ");
+        int res = mayo2(arr, 0, n-1);
+        System.out.println("{8,3,10,5,7}");
+        System.out.println("el 2do mayor es : " + res);
 
     }
     
